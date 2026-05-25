352fe19423c286eb73fddd54a846d2687193bcf5
nishadZC <vinayak.nishad@zapcg.com>
2026-05-25 11:33:15 +0000

    Changes made by company user

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,9 +3,9 @@ import "./about.css"
 const About = () => {
   return (
     <section id="about" className="about-section" >
-      <h2>About</h2>
+      <h2>About EventManagement</h2>
       <p>
-        At Eventify, we specialize in professional event management, bringing creativity,
+        At Eventify, we specialized in professional event management, bringing creativity,
         precision, and seamless execution to every occasion. Whether it’s a wedding,
         corporate event, birthday, or product launch, our team is dedicated to making your
         event unforgettable.
