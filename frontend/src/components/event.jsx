2de68dcb85417e2ea5779de8fd8cc3188d0050c2
VinayakNishad <mca2024.vinayak@unigoaa.ac.in>
2026-06-07 15:34:26 +0000

    fix image rendering and made responsive

--- frontend/src/components/event.jsx
+++ frontend/src/components/event.jsx
@@ -13,7 +13,7 @@ const Event = (props) => {
       <div className="list_of_event">
         <Link to="/getStudios" onClick={()=>{props.setService("Photographer")}} className="event-card">
           <img
-            src="https://media.licdn.com/dms/image/D4D12AQGPsGZPrLyRow/article-cover_image-shrink_600_2000/0/1691669661170?e=2147483647&v=beta&t=8DeCPVCH3nkxH5dMfiLEWjK3xdq4TkGN372pnld548k"
+            src="https://media.istockphoto.com/id/610259354/photo/young-woman-using-dslr-camera.jpg?s=612x612&w=0&k=20&c=gjAR4JiqA8lkGQzssSrXxo3yl-cwr5j7Hy47cy-10c4="
             alt="Photographer"
           />
           <span className="event-title">Photographer</span>
@@ -29,8 +29,8 @@ const Event = (props) => {
 
         <Link to="/getStudios" onClick={()=>props.setService("Caterer")} className="event-card">
           <img
-            src="https://media.licdn.com/dms/image/D4D12AQGPsGZPrLyRow/article-cover_image-shrink_600_2000/0/1691669661170?e=2147483647&v=beta&t=8DeCPVCH3nkxH5dMfiLEWjK3xdq4TkGN372pnld548k"
-            alt="Photographer"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcxDDy1QxcXhYj7ALL3etnTgBPO-AEV4Pd5Q&s"
+            alt="Caterer"
           />
           <span className="event-title">Caterer</span>
         </Link>
@@ -56,7 +56,7 @@ const Event = (props) => {
       <div className="list_of_event">
         <Link to="/get_package" onClick={() => {props.setPackageType("Wedding");}} className="event-card">
           <img
-            src="https://media.licdn.com/dms/image/D4D12AQGPsGZPrLyRow/article-cover_image-shrink_600_2000/0/1691669661170?e=2147483647&v=beta&t=8DeCPVCH3nkxH5dMfiLEWjK3xdq4TkGN372pnld548k"
+            src="https://images.stockcake.com/public/f/a/7/fa76812e-16a2-4a87-8ead-b78b726c9636_large/traditional-indian-wedding-stockcake.jpg"
             alt="Wedding"
           />
           <span className="event-title">Wedding</span>
@@ -72,7 +72,7 @@ const Event = (props) => {
 
         <Link to="/get_package" onClick={() => {props.setPackageType("Anniversary");}} className="event-card">
           <img
-            src="https://media.licdn.com/dms/image/D4D12AQGPsGZPrLyRow/article-cover_image-shrink_600_2000/0/1691669661170?e=2147483647&v=beta&t=8DeCPVCH3nkxH5dMfiLEWjK3xdq4TkGN372pnld548k"
+            src="https://www.gudlu.in/img/gallery/anniversary-celebration-in-gudlu-resort-chikmagaluru.webp"
             alt="Anniversary"
           />
           <span className="event-title">Anniversary</span>
@@ -80,7 +80,7 @@ const Event = (props) => {
 
         <Link to="/get_package" onClick={() => {props.setPackageType("Baby Shower");}} className="event-card">
           <img
-            src="https://media.licdn.com/dms/image/D4D12AQGPsGZPrLyRow/article-cover_image-shrink_600_2000/0/1691669661170?e=2147483647&v=beta&t=8DeCPVCH3nkxH5dMfiLEWjK3xdq4TkGN372pnld548k"
+            src="https://www.funkypigeon.com/blog/wp-content/uploads/2025/01/baby-shower.jpg"
             alt="Baby Shower"
           />
           <span className="event-title">Baby Shower</span>
